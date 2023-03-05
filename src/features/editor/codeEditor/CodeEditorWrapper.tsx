bd790d8fb68be34e07cc4d13465695a275311312
Mario <marioskrlec222@gmail.com>
2023-03-05 07:13:25 +0100

    Fixed bug with updating on mount

--- src/features/editor/codeEditor/CodeEditorWrapper.tsx
+++ src/features/editor/codeEditor/CodeEditorWrapper.tsx
@@ -12,7 +12,6 @@ export function CodeEditorWrapper() {
 
 	useEffect(() => {
 		CachedContentSubscriber.create().subscribe('tab_change', (name, value) => {
-			console.log(isCachedContentEvent(value), value);
 			if (isCachedContentEvent(value)) {
 				setSelectedFile(undefined);
 
@@ -24,27 +23,23 @@ export function CodeEditorWrapper() {
 		});
 	}, []);
 
-	useEffect(() => {
-		if (selectedFile) {
-			updateContent({
-				fileId: selectedFile.id,
-				projectId: selectedFile.projectId,
-				content: content,
-			});
-
-			CachedContentSubscriber.create().updateCache(selectedFile.id, {
-				id: selectedFile.id,
-				projectId: selectedFile.projectId,
-				content: content,
-				userId: selectedFile.userId,
-			});
-		}
-	}, [content, selectedFile]);
-
 	const onChange = useCallback(
 		debounce(500, (text: string) => {
 			if (selectedFile) {
 				setContent(text);
+
+				updateContent({
+					fileId: selectedFile.id,
+					projectId: selectedFile.projectId,
+					content: content,
+				});
+
+				CachedContentSubscriber.create().updateCache(selectedFile.id, {
+					id: selectedFile.id,
+					projectId: selectedFile.projectId,
+					content: content,
+					userId: selectedFile.userId,
+				});
 			}
 		}),
 		[selectedFile],
