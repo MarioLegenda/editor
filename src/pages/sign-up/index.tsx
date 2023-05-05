eb4e081ba5f7818df54adfd681de52e56d397751
Mario <marioskrlec222@gmail.com>
2023-05-05 15:21:50 +0200

    Fixed bug with sign up

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -5,7 +5,7 @@ import { useAuthState } from '@/lib/stateManagement/auth/getters';
 import { AuthState } from '@/lib/stateManagement/types/authState';
 
 export default function Index() {
-	useRedirectIfSignedIn('/editor');
+	useRedirectIfSignedIn('/editor/projects');
 	const authState = useAuthState();
 
 	return <Layout>{authState() === AuthState.SIGNED_OUT && <Main />}</Layout>;
